0a636668dfbfc1c805d42eb1a6b6a323653ff1fb
mirmannan99 <mirmannan99@gmail.com>
2023-01-20 20:49:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import KnightMoves from "./KnightMove";
+
+function App() {
+  return (
+    <div className="App">
+      <KnightMoves />
+    </div>
+  );
+}
+
+export default App;
